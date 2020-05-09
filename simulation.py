fa7fa31042d5d6fed6281ef2ebc2f52d43dd7707
Shicong Xie <xies@stanford.edu>
2020-05-09 16:23:50 -0700

    simulation advancement mostly working
    not working: rb conc transition lookup

--- simulation.py
+++ simulation.py
@@ -7,6 +7,7 @@ Created on Wed Jan 22 01:28:07 2020
 """
 
 import numpy as np
+from numpy import random
 
 #def initialize_cell_empirical_kde(kde):
 #    '''
@@ -24,24 +25,33 @@ def advance_dt(cell,params):
     '''
     # @todo: 1 grow size 2) synthesize rb
     next_cell = cell.copy()
-    next_cell['Time'] += 1
+    next_cell['Time'] += 1/6.
     
-    # check g1/s
-    if g1s_transition(cell,params):
-        next_cell['Phase'].values = 'G2'
+    # First calculate growths
+    drb = rb_synthesis_lut(cell,params)
+    ds = size_synthesis_lut(next_cell,params)
     
-    # check division
-    if division(cell,params):
-        next_cell['RB'] = np.nan
-        next_cell['Size'] = np.nan
-        next_cell['Phase'] = 'None'
-    else:
-        # If no division, then calculate next step
-        drb = rb_synthesis_lut(next_cell,params)
-        ds = size_synthesis_lut(next_cell,params)
+    next_cell['Size'] += ds
+    next_cell['RB'] += drb
+    next_cell['RB conc'] = next_cell['RB'] / next_cell['Size']
+    
+    if cell['Phase'].values[0] == 'G2':
         
-        next_cell['Size'] += ds
-        next_cell['RB'] += drb
+        # check division
+        if division(cell,params):
+            next_cell['RB'] = np.nan
+            next_cell['Size'] = np.nan
+            next_cell['RB conc'] = np.nan
+            next_cell['Phase'] = 'None'
+            
+            return next_cell
+            
+    elif cell['Phase'].values[0] == 'G1':
+        # check g1/s
+        if g1s_transition(cell,params):
+            print('G1S transitioned')
+            next_cell['Phase'] = 'G2'
+            
     
     return next_cell
 
@@ -73,9 +83,18 @@ def size_synthesis_lut(cell,params):
     
     
 def g1s_transition(cell,params):
+    
+    transitioned = False
+    
     if cell['Phase'].values == 'G1':
         # @todo: build logit lookup table baesed on [RB]
-        if cell['RB'].values/cell['Size'].values:
+        rb_conc = cell['RB'] / cell['Size']
+        m = params.loc['RB conc']['G1S trans m']
+        b = params.loc['RB conc']['G1S trans b']
+        p = np.polyval([m,b], rb_conc)
+        
+        q = random.rand(1)[0]
+        if q < p:
             transitioned = True
         else:
             transitioned = False
@@ -87,7 +106,7 @@ def g1s_transition(cell,params):
 def division(cell,params):
     # @todo: currently don't implement daughters div == death
     
-    if cell['Size'] > cell['Birth size']/2:
+    if cell['Size'].values > cell['Birth size'].values/2:
         return True
     else:
         return False
@@ -103,6 +122,7 @@ def mc_accept(prob_lut,trial_value):
     
     p = rand(1)[0]
     
-    I = find_nearest_idx(prob_lut,trial_value)
+    prob_lut
+    # I = find_nearest_idx(prob_lut,trial_value)
     
     return p < I # acceptance rate
--- simulation_script.py
+++ simulation_script.py
@@ -15,36 +15,53 @@ from numpy import random
 from scipy import ndimage, stats
 from scipy.interpolate import UnivariateSpline
 import statsmodels.api as sm
-
+import simulation
 
 # 1) Initial conditions / parameters
-Ncells = 10
-Niter = 50
+Ncells = 1
+Niter = 100
 
 emparams = pd.read_pickle('/Users/xies/Box/HMECs/RB toy model/emp_parameters.pkl')
 trans_params = pd.read_pickle('/Users/xies/Box/HMECs/RB toy model/trans_params.pkl')
-emparams = emparams.append(trans_params)
+
+emparams = emparams.join(trans_params)
+emparams.at['RB conc','G1S trans m'] = trans_params.loc['RB conc','G1S trans m']
+emparams.at['RB conc','G1S trans b'] = trans_params.loc['RB conc','G1S trans b']
 
 
+#%%
 # Initialize cells as dictionaries
 population = np.empty((Ncells,Niter+1),dtype=dict)
 for i in range(Ncells):
 
     # @todo: fix zero size cells
-    init_size = random.normal(loc = emparams.loc['Size','Mean birth'],scale = emparams.loc['Size','Std birth'])
+    init_size = random.normal(loc = emparams.loc['Size','Mean birth'],scale = emparams.loc['Size','Std birth']/3)
     init_rb = random.normal(loc = emparams.loc['RB','Mean birth'],scale = emparams.loc['RB','Std birth'])
-    cell = pd.DataFrame(data = [[i,0,init_size,init_rb,'G1',init_size]],
-                         columns = ['CellID','Time','Size','RB','Phase','Birth size'])
+    rb_conc = init_rb / init_size
+    cell = pd.DataFrame(data = [[i,0,init_size,init_rb,rb_conc,'G1',init_size]],
+                         columns = ['CellID','Time','Size','RB','RB conc','Phase','Birth size'])
     population[i,0] = cell
+    
+#%%
     
 # Start stimulation
 for t in np.arange(Niter)+1:
     for i in range(Ncells):
-
-        this_cell_now = population[i,t-1]
-        this_cell_next = advance_dt(cell,emparams)
+        
+        print t
+        
+        # Go to previous time point and grab cell to work on
+        this_cell_now = population[i,t-1].copy()
+        
+        # Check if cell divided
+        if this_cell_now['Phase'].values == 'None':
+            this_cell_next = this_cell_now
+        else:
+            this_cell_next = simulation.advance_dt(this_cell_now,emparams)
         
         population[i,t] = this_cell_next
 
-
-
+        
+        
+        
+        
